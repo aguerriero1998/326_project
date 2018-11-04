fb34c4dc507453f34f42ce3018291334a6df0080
christopherdoan <chrdoan@umass.edu>
2018-11-04 19:47:10 +0000

    updated both urls.py and views.py

--- src/project2/team_t/inspire/views.py
+++ src/project2/team_t/inspire/views.py
@@ -3,45 +3,27 @@ from inspire.models import Course, CourseInstance, Professor, Student, Days
 from django.views import generic
 
 # Create your views here.
-def dashboard(request):
-    """View function for home page of site.
-    # Generate counts of some of the main objects
-    num_books = Book.objects.all().count()
-    num_instances = BookInstance.objects.all().count()
-
-    # Available books (status = 'a')
-    num_instances_available = BookInstance.objects.filter(
-        status__exact="a").count()
-
-    # The 'all()' is implied by default.
-    num_authors = Author.objects.count()
+def dashboard(request, idnumber):
+    def get_list(day):
+        courses_taking = CourseInstance.objects.filter(students = idnumber)
+        courses_taking = courses_taking.filter(days = day)
+        listOfdicts = []
+        length = range(len(courses_taking))
+        for i in length:
+            dict = {"key": courses_taking[i].name,
+            "room": courses_taking[i].location,
+            "start": courses_taking[i].time,
+            "end": courses_taking[i].time}
+            listOfdicts.append(dict)
+        return listOfdicts
 
     context = {
-        "num_books": num_books,
-        "num_instances": num_instances,
-        "num_instances_available": num_instances_available,
-        "num_authors": num_authors,
+        "monday_classes": get_list("monday"),
+        "tuesday_classes": get_list("tuesday"),
+        "wednesday_classes": get_list("wednesday"),
+        "thursday_classes": get_list("thursday"),
+        "friday_classes": get_list("friday"),
     }
-    """
-    
-    """def get_list(day):
-        student = Student.objects.filter(idnumber = self.idnumber)
-        course_taking = student.objects
-        listOfdicts = []
-        length = range(len(q1))
-        for i in length:
-            dict = {"key": q1[i].name, }
-            listOfdicts.append(dict) """
-    
-    context = {}
-
-    """context = {
-        monday_classes = get_list("monday")
-        tuesday_classes = get_list("tuesday")
-        wednesday_classes = get_list("wednesday")
-        thursday_classes = get_list("thursday")
-        friday_classes = get_list("friday")
-    }"""
     # Render the HTML template index.html with the data in the context variable
     return render(request, "dashboard.html", context=context)
 
@@ -70,4 +52,24 @@ class profDetailView(generic.DetailView):
 class courseDetailView(generic.DetailView):
     model = Course
     template_name = "course-description.html"
+
+    """View function for home page of site.
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(
+        status__exact="a").count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+    }
+    """
 
