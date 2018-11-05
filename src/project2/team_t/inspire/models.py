a1415c3edd37c736223ec8b867d49b05224198ef
Alex Guerriero <aguerriero@umass.edu>
2018-11-05 10:55:39 -0500

    Model agrees with templates and templates are working

--- src/project2/team_t/inspire/models.py
+++ src/project2/team_t/inspire/models.py
@@ -12,10 +12,9 @@ class Course(models.Model):
     description =  models.TextField(max_length=100)
     credits =  models.IntegerField(validators=[MaxValueValidator(4), MinValueValidator(1)])
     rating =  models.DecimalField(max_digits=3, decimal_places=2)
-    comments =  models.TextField(max_length=100)
     gened =  models.CharField(max_length=3)
     major =  models.CharField(max_length=25)
-    reccomendation =  models.ForeignKey('Course', on_delete=models.SET_NULL, null=True, blank = True)
+    reccomendation =  models.ForeignKey('CourseInstance', on_delete=models.SET_NULL, null=True, blank = True)
     
     
     def __str__(self):
@@ -26,9 +25,8 @@ class CourseInstance(models.Model):
     
     name = models.CharField(max_length=20, help_text='Enter a class name')
     prof = models.ForeignKey('Professor', on_delete=models.SET_NULL, null=True)
-    classnumber = models.IntegerField(validators=[MaxValueValidator(1000), MinValueValidator(1)])
-    time = models.TimeField(null=True, blank=True)
-    prerequisites = models.ForeignKey('Course', on_delete=models.SET_NULL, null=True, related_name='prerequisites', blank = True)
+    classnumber = models.IntegerField(primary_key=True, validators=[MaxValueValidator(1000), MinValueValidator(1)])
+    prerequisites = models.ForeignKey('Course', on_delete=models.SET_NULL, null=True, related_name='prerequisites', blank = True) # TO RECONSIDER ....
     start = models.TimeField(null=True, blank=True) 
     end = models.TimeField(null=True, blank=True)
     semester = models.CharField(max_length=6)
@@ -44,10 +42,9 @@ class CourseInstance(models.Model):
         return self.name
 
 class Professor(models.Model):
-    name = models.CharField(max_length=20, help_text='Enter a name')
-    taught = models.ForeignKey('CourseInstance', on_delete=models.SET_NULL, null=True)
+    name = models.CharField(primary_key=True, max_length=20, help_text='Enter a name')
     review = models.ManyToManyField('Reviews')
-    rating =  models.DecimalField(max_digits=3, decimal_places=2)
+    rating =  models.DecimalField(max_digits=3, decimal_places=2, null=True)
     
     def __str__(self):
         """String for representing the Model object."""
@@ -61,9 +58,9 @@ class Student(models.Model):
     gender = models.CharField(max_length=25)
     pronouns = models.CharField(max_length=25)
     emergency = models.CharField(max_length=25)
-    coursestaken = models.ForeignKey('Course', on_delete=models.SET_NULL, null=True)
+    coursestaken = models.ManyToManyField('CourseInstance')
     coursesnow = models.ManyToManyField("CourseInstance", related_name='coursesnow')
-    shoppingcart = models.ForeignKey('CourseInstance', on_delete=models.SET_NULL, null=True, related_name='shoppingcart')
+    shoppingcart = models.ManyToManyField('CourseInstance', related_name='shoppingcart')
 
     def __str__(self):
         """String for representing the Model object."""
@@ -71,15 +68,15 @@ class Student(models.Model):
 
 class Days(models.Model):
     OFFERED = (
-            ("m", "Monday"),
-            ("tu", "Tuesday"),
-            ("w", "Wednesday"),
-            ("th", "Thursday"),
-            ("f", "Friday"),
+            ("Mon", "Monday"),
+            ("Tu", "Tuesday"),
+            ("Wed", "Wednesday"),
+            ("Th", "Thursday"),
+            ("Fri", "Friday"),
     )
 
     daysoffered = models.CharField(
-            max_length=2,
+            max_length=3,
             choices=OFFERED,
             blank=True,
             default="m",
@@ -93,3 +90,17 @@ class Days(models.Model):
 class Reviews(models.Model):
     remarks = models.TextField(max_length=200)
     giver = models.ForeignKey('Student', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.remarks[0:30]
+
+
+class CourseReview(models.Model):
+    remarks = models.TextField(max_length=200)
+    giver = models.ForeignKey('Student', on_delete=models.SET_NULL, null=True)
+    course = models.ForeignKey('Course', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.remarks
