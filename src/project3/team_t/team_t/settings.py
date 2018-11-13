0df904ec7b3118436e24f57a14d3a4e9f121a127
connor1733 <cjreardon@umass.edu>
2018-11-13 21:50:11 +0000

    Added passsword reset implementation

--- src/project3/team_t/team_t/settings.py
+++ src/project3/team_t/team_t/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Email for password reset
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
